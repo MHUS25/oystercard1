fc1b811bed786787be65cacf340c22c0319465ba
Muna <mmhussein125@gmail.com>
2018-10-08 14:24:03 +0100

    Added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,12 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+    it 'it updates balance with amount' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
+
 end
